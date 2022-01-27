d99519aa54ba71894610f6a50a58f00934db1d48
Thelethalghost <71647167+Thelethalghost@users.noreply.github.com>
2022-01-27 10:12:30 +0530

    Update createtable.py

--- createtable.py
+++ createtable.py
@@ -1,4 +1,3 @@
-# the hostel names are from my school "RAJGHAT BESANT SCHOOL" and the data of students is fake
 import mysql.connector as mc
 import csv
 
@@ -8,36 +7,52 @@ db = mc.connect(
     passwd = "root",
     database = "Hostels"
 )
+
 myc = db.cursor()
+
+myc.execute("SELECT * from hostels") # do not comment out this query
+hostels_names = [x[1] for x in myc]
 def createdatabase():
     myc.execute("CREATE DATABASE Hostels")
 def createtables():
     myc.execute("CREATE TABLE Students(Adminno int PRIMARY KEY,Name varchar(50), Class smallint UNSIGNED,Father_name varchar(100), Mother_name varchar(100), DOB Varchar(15),Phone_number int UNSIGNED,Email varchar(100), Age int,Adress varchar(200), Hostel_ID int)")
+
     myc.execute("CREATE TABLE Hostels(Hostel_ID int PRIMARY KEY, Name varchar(20))")
-    myc.execute("CREATE TABLE Sneh_Sadan(Adminno int PRIMARY KEY,Student_name varchar(50), Age int,class smallint UNSIGNED, Hostel_ID int, FOREIGN KEY (Adminno) REFERENCES Students(Adminno) ON UPDATE CASCADE ON DELETE CASCADE)")
-    myc.execute("CREATE TABLE RR_House(Adminno int PRIMARY KEY,Student_name varchar(50), Age int,class smallint UNSIGNED, Hostel_ID int, FOREIGN KEY (Adminno) REFERENCES Students(Adminno) ON UPDATE CASCADE ON DELETE CASCADE)")
-    myc.execute("CREATE TABLE East_House(Adminno int PRIMARY KEY,Student_name varchar(50), Age int,class smallint UNSIGNED, Hostel_ID int, FOREIGN KEY (Adminno) REFERENCES Students(Adminno) ON UPDATE CASCADE ON DELETE CASCADE)")
-    myc.execute("CREATE TABLE West_House(Adminno int PRIMARY KEY,Student_name varchar(50), Age int,class smallint UNSIGNED, Hostel_ID int, FOREIGN KEY (Adminno) REFERENCES Students(Adminno) ON UPDATE CASCADE ON DELETE CASCADE)")
-    myc.execute("CREATE TABLE North_House(Adminno int PRIMARY KEY,Student_name varchar(50), Age int,class smallint UNSIGNED, Hostel_ID int, FOREIGN KEY (Adminno) REFERENCES Students(Adminno) ON UPDATE CASCADE ON DELETE CASCADE)")
-    myc.execute("CREATE TABLE South_House(Adminno int PRIMARY KEY,Student_name varchar(50), Age int,class smallint UNSIGNED, Hostel_ID int, FOREIGN KEY (Adminno) REFERENCES Students(Adminno) ON UPDATE CASCADE ON DELETE CASCADE)")
-    myc.execute("CREATE TABLE Gokul(Adminno int PRIMARY KEY,Student_name varchar(50), Age int,class smallint UNSIGNED, Hostel_ID int, FOREIGN KEY (Adminno) REFERENCES Students(Adminno) ON UPDATE CASCADE ON DELETE CASCADE)")
-    myc.execute("CREATE TABLE Shanti_vihar(Adminno int PRIMARY KEY,Student_name varchar(50), Age int,class smallint UNSIGNED, Hostel_ID int, FOREIGN KEY (Adminno) REFERENCES Students(Adminno) ON UPDATE CASCADE ON DELETE CASCADE)")
-    myc.execute("CREATE TABLE Padma_Kutir(Adminno int PRIMARY KEY,Student_name varchar(50), Age int,class smallint UNSIGNED, Hostel_ID int, FOREIGN KEY (Adminno) REFERENCES Students(Adminno) ON UPDATE CASCADE ON DELETE CASCADE)")
-    myc.execute("CREATE TABLE Varuna(Adminno int PRIMARY KEY,Student_name varchar(50), Age int,class smallint UNSIGNED, Hostel_ID int, FOREIGN KEY (Adminno) REFERENCES Students(Adminno) ON UPDATE CASCADE ON DELETE CASCADE)")
-    myc.execute("CREATE TABLE Nilgiri(Adminno int PRIMARY KEY,Student_name varchar(50), Age int,class smallint UNSIGNED, Hostel_ID int, FOREIGN KEY (Adminno) REFERENCES Students(Adminno) ON UPDATE CASCADE ON DELETE CASCADE)")
-    myc.execute("CREATE TABLE River_Side_Hostel(Adminno int PRIMARY KEY,Student_name varchar(50), Age int,class smallint UNSIGNED, Hostel_ID int, FOREIGN KEY (Adminno) REFERENCES Students(Adminno) ON UPDATE CASCADE ON DELETE CASCADE)")
-#     BECAUSE STUDENTS TABLE WILL NOT BE CREATED WITHOUT THE HOSTEL TABLE ALREADY CREATED THEREFORE EITHER WE CAN ALTER AS IN THIS CASE OR CREATE THE HOSTEL TABLE BEFORE STUDENTS TABLE
-    myc.execute("ALTER TABLE Students ADD  FOREIGN KEY (Hostel_ID) REFERENCES Hostels(Hostel_ID) ON UPDATE CASCADE ON DELETE CASCADE")
 
-def search_data():
-    sea_no = int(input("Enter adminno. of the student you want to search the data for: "))
+def add_data_hostels():
+    
+    global hostels_names
 
     
-    
-    
-    
+
+    while True:
+
+        hostelid = int(input("Enter the hostel id: "))                
+        name = input("Enter Hostel name: ")
+        myc.execute(f"INSERT INTO Hostels(Hostel_ID,Name) values(%s,%s)",(hostelid,name))
+        # hostels_names.append(name)
+        c = input('Another data(Y/n): ').lower()
+        if c == 'n':
+            break
+    db.commit()
+
+
+def add_data():
+    while True:
+        ask = input("Enter the name of the hostel name in which you want add the student: ")
+        hotelid = myc.execute(f"SELECT Hostel_ID FROM Hostels WHERE Name = {ask}")
+        adminno = int(input("Enter the admission number of the student: "))
+        name = input("Enter the name of the student: ")
+        age = int(input("Enter the age of the student: "))
+        classe = int(input("Enter the class of the student: "))
+        pass
+
+# myc.execute("ALTER TABLE Students ADD FOREIGN KEY (Hostel_ID) REFERENCES Hostels(Hostel_ID)")
+
 # createdatabase()
 
-
-
 # createtables()
+
+# add_data_hostels()
+
+print(hostels_names)
