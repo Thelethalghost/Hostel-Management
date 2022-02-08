0b372842daa5cb3684a680ed2ee0c23df5f335c0
Thelethalghost <71647167+Thelethalghost@users.noreply.github.com>
2022-02-08 10:42:59 +0530

    Update startup3.py

--- startup3.py
+++ startup3.py
@@ -19,9 +19,9 @@ hostels_id = [x[0] for x in myc]
 def create_tables_hostels():
     for name in hostels_names:
         # tup = (name,)
-        myc.execute(f"CREATE TABLE {name}(Adminno int PRIMARY KEY,Student_name varchar(50),Age smallint ,Class smallint ,Gender char(10),Hostel_ID int,FOREIGN KEY (Adminno) REFERENCES Students(Adminno), FOREIGN KEY(Hostel_ID) REFERENCES Hostels(Hostel_ID))")
+        myc.execute(f"CREATE TABLE {name}(Adminno int PRIMARY KEY,Student_name varchar(50),Age smallint ,Class smallint ,Gender char(10),Hostel_ID int,FOREIGN KEY (Adminno) REFERENCES Students(Adminno), FOREIGN KEY(Hostel_ID) REFERENCES Hostels(Hostel_ID) ON UPDATE CASCADE ON DELETE CASCADE)")
         db.commit()
-    myc.execute("ALTER TABLE Students ADD FOREIGN KEY(Hostel_ID) REFERENCES Hostels(Hostel_ID)")
+    myc.execute("ALTER TABLE Students ADD FOREIGN KEY(Hostel_ID) REFERENCES Hostels(Hostel_ID) ON UPDATE CASCADE ON DELETE CASCADE")
 
 def add_data_students():
     studata = pd.read_csv(r'D:\Pranav\Studentsfakedata.csv',index_col=False, delimiter=',')
