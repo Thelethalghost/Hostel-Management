af79772abd38a15754f8b3ce7b7726a910533e50
Thelethalghost <71647167+Thelethalghost@users.noreply.github.com>
2022-03-01 09:32:04 +0530

    Update startup2.py

--- startup2.py
+++ startup2.py
@@ -1,47 +1,77 @@
 import mysql.connector as mc
 from nameandpass import name,passwd
+import startup3
 
-db = mc.connect(
-    host = 'localhost',
-    user = {name},
-    passwd = {passwd},
-    database = 'hostels'
-)
+def run():
 
-myc = db.cursor(buffered=True)
+    db = mc.connect(
+        host = 'localhost',
+        user = name,
+        passwd = passwd,
+        database = 'Hostels_Management'
+    )
 
-def createtables():
-    myc.execute(
-        "CREATE TABLE Students(Adminno int PRIMARY KEY,Name varchar(50),Age int UNSIGNED,Class smallint UNSIGNED,Sex char(10),Father_name varchar(100), Mother_name varchar(100), DOB Varchar(15),Phone_number bigint UNSIGNED,Email varchar(100), Hostel_ID int)")
+    myc = db.cursor(buffered=True)
 
-    myc.execute("CREATE TABLE Hostels(Hostel_ID int PRIMARY KEY, Name varchar(20))")
+    def createtables():
+        myc.execute(
+            "CREATE TABLE Students(Adminno int PRIMARY KEY,Name varchar(50),Age int UNSIGNED,Class smallint UNSIGNED,Sex char(10),Father_name varchar(100), Mother_name varchar(100), DOB Varchar(15),Phone_number bigint UNSIGNED,Email varchar(100), Hostel_ID int)")
+        myc.execute("CREATE TABLE Hostels(Hostel_ID int PRIMARY KEY, Name varchar(20))")
 
-def add_data_hostels():
+    def add_data_hostels():
 
-    while True:
+        while True:
 
-        hostelid = int(input("Enter the hostel id: "))
-        name = input("Enter Hostel name(Please refrainf from using spaces. use underscore(_) instead): ")
-        myc.execute(f"INSERT INTO Hostels(Hostel_ID,Name) values(%s,%s)", (hostelid, name))
-        c = input('Another data(Y/n): ').lower()
-        if c == 'n':
-            break
+            hostelid = int(input("Enter the hostel id: "))
+            name = input("Enter Hostel name(Please refrainf from using spaces. use underscore(_) instead): ")
+            myc.execute(f"INSERT INTO Hostels(Hostel_ID,Name) values(%s,%s)", (hostelid, name))
+            db.commit()
+            c = input('Another data(Y/n): ').lower()
+            if c == 'n':
+                break
 
-    db.commit()
+    def create_tables_hostels():
+        myc.execute("SELECT * from hostels")
+        hostels_names = [x[1] for x in myc]
+        myc.execute("SELECT * from hostels")
+        hostels_id = [x[0] for x in myc]
 
-try:
-    createtables()
+        for na in hostels_names:
+            # tup = (name,)
+            myc.execute(f"CREATE TABLE {na}(Adminno int PRIMARY KEY,Student_name varchar(50),Age smallint ,Class smallint ,Gender char(10),Hostel_ID int")
+        db.commit()
+
+    def checkup():
+        check = input('Enter Data changed(Y/n): ').lower()
+        if check == 'y':
+            startup3.run()
+        else:
+            print("Please change the data")
+            checkup()
 
     try:
-        add_data_hostels()
-        print(
-            'Please make sure that hostel id field in the csv file has only the numbers you have entered now. If not please change it')
-    except:
-        print(
-            'Please make sure that hostel id field in the csv file has only the numbers you have entered now. If not please change it')
-except Exception:
-    print('Tables Already created. Run startup3.py')
-    myc.execute('SELECT Hostel_ID FROM Hostels')
-    hostelids = [x[0] for x in myc]
-    print(hostelids)
-    print('Please make sure that hostel ids in the studentsfakedata.csv file is in the range of number that are printed above')
+        createtables()
+
+        try:
+            add_data_hostels()
+            create_tables_hostels()
+            print(
+                'Please make sure that hostel id field in the csv file has only the numbers you have entered now. If not please change it')
+            checkup()
+        except:
+            try:
+                create_tables_hostels()
+                print(
+                    'Please make sure that hostel id field in the csv file has only the numbers you have entered now. If not please change it')
+                checkup()
+            except:
+                print(
+                    'Please make sure that hostel id field in the csv file has only the numbers you have entered now. If not please change it')
+                checkup()
+    except Exception:
+        print('Tables Already created. Run startup3.py')
+        myc.execute('SELECT Hostel_ID FROM Hostels')
+        hostelids = [x[0] for x in myc]
+        print(hostelids)
+        print('Please make sure that hostel ids in the studentsfakedata.csv file is in the range of number that are printed above')
+        checkup()
